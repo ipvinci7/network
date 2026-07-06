4235bb7c7b4c767808a2daf03bafeaa7af3016c5
ipvinci7 <ipvinci7@gmail.com>
2026-07-06 19:11:11 +0100

    t

---
+++ pan/bluetooth.c
@@ -0,0 +1,5 @@
+#include <stido.h>
+
+//this code uses a msater slave paradigm
+
+//the master detects freq,how long the transmission occurs,the address to use
--- point2point.c
+++ point2point.c
@@ -2,6 +2,25 @@
 //the idea of point to point is the message called packet is sent from src to destination
 //so the packet should have an address 
 //definite address definite dest
+/*START
+
+Initialize communication
+
+Create connection request packet
+
+Send connection request
+
+Wait for response
+
+IF response is ACCEPTED
+
+    Connection established
+
+ELSE
+
+    Terminate communication
+
+END IF*/
 struct packet
 {
   char src[10];
@@ -10,3 +29,18 @@ struct packet
 }
 
 
+int sendpacket(struct packet *p)
+{
+  //this function sends packet from the sender to the reciever and carrries the addres of the sender as well as the reciever
+  //before a packet is sent a hand  
+  if(packet)
+    {
+       
+    }
+}
+
+int receievepacket(struct packet *p)
+{
+  /// this function is to recieve a packet and 
+
+}
